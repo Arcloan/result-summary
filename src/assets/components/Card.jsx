3a1793db3dc76a4687f8e9b087e533d08dcedfeb
Arcloan <95delchris@gmail.com>
2025-02-16 16:03:09 +0100

    Final commit

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,3 +1,27 @@
-export function Card({ imgUrl, title, score }) {
-    
+const bgColors = {
+    red: "bg-(--color-red)/20",
+    yellow: "bg-(--color-yellow)/20",
+    green: "bg-(--color-green)/20",
+    blue: "bg-(--color-blue)/20",
+}
+
+const textColors = {
+    red: "text-(--color-red)",
+    yellow: "text-(--color-yellow)",
+    green: "text-(--color-green)",
+    blue: "text-(--color-blue)",
+}
+
+export function Card({ imgUrl, title, score, color }) {
+    return (
+        <div className={`p-2 flex justify-between ${bgColors[color]} rounded`} >
+            <div className="category flex gap-2">
+                <img src={imgUrl} alt="" />
+                <p className={textColors[color]}>{title}</p>
+            </div>
+            <p className="text-darkGrayBlue/40">
+                <span className="text-darkGrayBlue">{score}</span> / 100
+            </p>
+        </div>
+    )
 }
--- src/assets/components/Summary.jsx
+++ src/assets/components/Summary.jsx
@@ -4,11 +4,11 @@ export function Summary() {
     return (
         <div className="p-6 grid gap-4">
             <h3 className="text-(--color-darkGrayBlue)">Summary</h3>
-            <Card imgUrl="../icon-reaction.svg" title="Reaction" score={80}></Card>
-            <Card imgUrl="../icon-memory.svg" title="Memory" score={92}></Card>
-            <Card imgUrl="../icon-verbal.svg" title="Verbal" score={61}></Card>
-            <Card imgUrl="../icon-visual.svg" title="Visual" score={73}></Card>
-            <button className="p-3 rounded-full block text-center bg-(--color-darkGrayBlue) text-white">Continue</button>
+            <Card imgUrl="/icon-reaction.svg" title="Reaction" score={80} color="red"></Card>
+            <Card imgUrl="/icon-memory.svg" title="Memory" score={92} color="yellow"></Card>
+            <Card imgUrl="/icon-verbal.svg" title="Verbal" score={61} color="green"></Card>
+            <Card imgUrl="/icon-visual.svg" title="Visual" score={73} color="blue"></Card>
+            <button className="p-3 rounded-full block text-center h-max mt-auto bg-(--color-darkGrayBlue) text-white hover:cursor-pointer hover:bg-gradient-to-b from-(--color-gradientBgStart) to-(--color-gradientBgEnd)">Continue</button>
         </div>
     )
 }
